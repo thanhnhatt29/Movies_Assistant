f15bf6488e5e1844877d29105d30e54b288c987d
Thanh Nhat <nhattpruu29122002@gmail.com>
2024-10-06 02:38:05 +0700

    flask app

---
+++ movies_assistant/db_prep.py
@@ -0,0 +1,115 @@
+import os
+import pandas
+from tqdm.auto import tqdm
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch, helpers
+from db import init_db
+
+
+os.environ['RUN_TIMEZONE_CHECK'] = '0'
+
+
+load_dotenv()
+
+
+ELASTIC_URL = os.getenv("ELASTIC_URL_LOCAL")
+INDEX_NAME = os.getenv("INDEX_NAME")
+
+len_documents = 722359
+
+
+def load_data(data_path='../data/movies.csv'):
+    df = pandas.read_csv(data_path, keep_default_na=False)
+    df['release_date'].replace('', '1970-01-01', inplace=True)
+
+    documents = df.to_dict(orient='records')
+    return documents
+
+
+def setup_elasticsearch(es_client):
+    print("Setting up Elasticsearch...")
+    
+
+    index_settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+        },
+        "mappings": {
+            "properties": {
+                "id": {"type": "integer", "null_value": 0},
+                "title": {"type": "text"},
+                "genres": {"type": "text"},
+                "original_language": {"type": "keyword"},
+                "overview": {"type": "text"},
+                "popularity": {"type": "float"},
+                "production_companies": {"type": "text"},
+                "release_date": {"type": "date", "format": "yyyy-MM-dd", "null_value": "1970-01-01"},
+                "budget": {"type": "float"},
+                "revenue": {"type": "float"},
+                "runtime": {"type": "float"},
+                "status": {"type": "keyword"},
+                "tagline": {"type": "text"},
+                "vote_average": {"type": "float"},
+                "vote_count": {"type": "float"},
+                "credits": {"type": "text"},
+                "keywords": {"type": "text"},
+            }
+        }
+    }
+
+    es_client.indices.create(index=INDEX_NAME, body=index_settings)
+    print(f"Elasticsearch index '{INDEX_NAME}' created")
+
+
+# Function to create the bulk actions
+def generate_actions(documents):
+    for doc in documents:
+        yield {
+            "_index": INDEX_NAME,
+            "_source": doc
+        }
+
+
+# Bulk indexing function with progress bar
+def bulk_index(es_client, documents, batch_size=2000):
+    total_documents = len(documents)
+    progress_bar = tqdm(total=total_documents, desc="Indexing Progress")
+
+    for success, info in helpers.parallel_bulk(
+        es_client,
+        generate_actions(documents),
+        chunk_size=batch_size
+    ):
+        if not success:
+            print('A document failed:', info)
+        progress_bar.update(batch_size)
+
+    progress_bar.close()
+
+
+def main():
+    
+    print("Initializing database...")
+    init_db()
+    
+    es_client = Elasticsearch(ELASTIC_URL, request_timeout=40)
+    
+    if es_client.indices.exists(index=INDEX_NAME) and es_client.count(index=INDEX_NAME).get('count') == len_documents:
+        print(f"Index '{INDEX_NAME}' already exists with {len_documents} documents.")
+        return
+    else:
+        try:
+            es_client.indices.delete(index=INDEX_NAME)
+        except Exception as e:
+            print(f"Error deleting index: {e}")
+    
+    print("Starting the indexing process...")
+    documents = load_data()
+    setup_elasticsearch(es_client)
+    bulk_index(es_client, documents)
+
+    print("Indexing process completed successfully!")
+
+if __name__ == "__main__":
+    main()
---
+++ movies_assistant/rag.py
@@ -0,0 +1,160 @@
+import os
+import json
+from time import time
+from dotenv import load_dotenv
+from huggingface_hub import InferenceClient
+from elasticsearch import Elasticsearch
+import ingest
+
+load_dotenv()
+
+ingest.load_index()
+HUGGINGFACE_TOKEN = os.getenv('HUGGINGFACE_TOKEN')
+es_client = Elasticsearch('http://localhost:9200')
+index_name = "movies"
+
+prompt_template = """
+You're a movie assistant. Answer the QUESTION based on the CONTEXT from our movies data.
+Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT:
+{context}
+""".strip()
+
+
+entry_template = """
+title: {title}
+genres: {genres}
+original_language: {original_language}
+overview: {overview}
+popularity: {popularity}
+production_companies: {production_companies} 
+release_date: {release_date}
+budget: {budget}
+revenue: {revenue}
+runtime: {runtime}
+status: {status}
+tagline: {tagline}
+vote_average: {vote_average}
+vote_count: {vote_count}
+credits: {credits}
+keywords: {keywords}
+""".strip()
+
+evaluation_prompt_template = """
+You are an expert evaluator for a RAG system.
+Your task is to analyze the relevance of the generated answer to the given question.
+Based on the relevance of the generated answer, you will classify it
+as "NON_RELEVANT", "PARTLY_RELEVANT", or "RELEVANT".
+
+Here is the data for evaluation:
+
+Question: {question}
+Generated Answer: {answer}
+
+Please analyze the content and context of the generated answer in relation to the question
+and provide your evaluation in parsable JSON without using code blocks:
+
+{{
+  "Relevance": "NON_RELEVANT" | "PARTLY_RELEVANT" | "RELEVANT",
+  "Explanation": "[Provide a brief explanation for your evaluation]"
+}}
+""".strip()
+
+
+def elastic_search(query):
+
+    search_query = {
+        "size": 10,
+        "query": {
+            "multi_match": {
+                "query": query,
+                "fields": ["title^3", "description^2", "overview^1.5", "genres", "keywords"],
+                "type": "best_fields",
+                "fuzziness": "AUTO"
+            }
+        }
+    }
+
+    response = es_client.search(index=index_name, body=search_query)
+
+    result_docs = [hit['_source'] for hit in response['hits']['hits']]
+
+    return result_docs
+
+
+def build_prompt(query, search_results):
+    context = ""
+
+    for doc in search_results:
+        context = context + entry_template.format(**doc) + "\n\n"
+
+    prompt = prompt_template.format(question=query, context=context).strip()
+    return prompt
+
+
+def llm(prompt, model='mistralai/Mixtral-8x7B-Instruct-v0.1'):
+    client = InferenceClient(
+        model,
+        token=HUGGINGFACE_TOKEN,
+    )
+
+    response = client.chat_completion(
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=500
+    )
+    
+    answer = response.choices[0].message.content
+    
+    token_stats = {
+        "prompt_tokens": response.usage.prompt_tokens,
+        "completion_tokens": response.usage.completion_tokens,
+        "total_tokens": response.usage.total_tokens,
+    }
+    
+    return answer, token_stats
+
+
+def evaluate_relevance(question, answer):
+    prompt = evaluation_prompt_template.format(question=question, answer=answer)
+    evaluation, tokens = llm(prompt)
+
+    try:
+        json_eval = json.loads(evaluation)
+        return json_eval, tokens
+    except json.JSONDecodeError:
+        result = {"Relevance": "UNKNOWN", "Explanation": "Failed to parse evaluation"}
+        return result, tokens
+
+def rag(query, model='mistralai/Mixtral-8x7B-Instruct-v0.1'):
+
+    t0 = time()
+
+    search_results = elastic_search(query)
+    prompt = build_prompt(query, search_results)
+    answer, token_stats = llm(prompt, model=model)
+
+    relevance, rel_token_stats = evaluate_relevance(query, answer)
+
+    t1 = time()
+    took = t1 - t0
+
+    answer_data = {
+        "answer": answer,
+        "model_used": model,
+        "response_time": took,
+        "relevance": relevance.get("Relevance", "UNKNOWN"),
+        "relevance_explanation": relevance.get(
+            "Explanation", "Failed to parse evaluation"
+        ),
+        "prompt_tokens": token_stats["prompt_tokens"],
+        "completion_tokens": token_stats["completion_tokens"],
+        "total_tokens": token_stats["total_tokens"],
+        "eval_prompt_tokens": rel_token_stats["prompt_tokens"],
+        "eval_completion_tokens": rel_token_stats["completion_tokens"],
+        "eval_total_tokens": rel_token_stats["total_tokens"]
+    }
+    
+    return answer_data
